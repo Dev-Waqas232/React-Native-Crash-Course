f033b2a272bca79d14421ef17b594b1bd55dbeb1
Waqas Munir <devwaqas232@gmail.com>
2024-09-16 12:07:44 +0500

    Completed File and folder structure setup

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Aora!</Text>
-      <Link href="/profile">Go to Profile</Link>
+      <Link href="/home">Go to Home</Link>
     </View>
   );
 }
